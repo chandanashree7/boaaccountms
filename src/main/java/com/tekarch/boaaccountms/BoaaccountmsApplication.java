be6766965bec4096fbe72135865dad1cde615d41
chandanashree9 <chandanashree9@yahoo.com>
2025-01-15 11:04:07 -0500

    boaaccountms committed

---
+++ src/main/java/com/tekarch/boaaccountms/BoaaccountmsApplication.java
@@ -0,0 +1,13 @@
+package com.tekarch.boaaccountms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoaaccountmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoaaccountmsApplication.class, args);
+	}
+
+}
